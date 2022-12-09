99b8f8d35b0fd5b08330a96adda4dc0c43d66b7d
J.s Ibitoye <jsibitoye@outlook.com>
2022-12-09 03:25:44 -0500

    Implemented progress with GUI

--- FlightReservation/src/ProgressBar.java
+++ FlightReservation/src/ProgressBar.java
@@ -1,24 +1,28 @@
-// This class creates the progress bar and prints to the console
-public class ProgressBar {
-    
-    private static final StringBuilder res = new StringBuilder();
-
-    static String progress(int pct) {
-        res.delete(0, res.length());
-        int numPounds = (pct + 9) / 10;
-        for (int i = 0 ; i != numPounds ; i++) {
-            res.append('#');
-        }
-        while (res.length() != 10) {
-            res.append(' ');
-        }
-        return res.toString();
-    }
-
-    public void loadBar() throws InterruptedException{
-        for (int i = 0 ; i <= 100 ; i+=Math.random() *8) {
-            Thread.sleep(50);
-            System.out.print(String.format("[%s]%d%%\r", progress(i), i));
-        }
-    }
-}
+/*
+ * used another class to performe the function.
+ * 
+ * 
+ * public class ProgressBar {
+ * 
+ * private static final StringBuilder res = new StringBuilder();
+ * 
+ * static String progress(int pct) {
+ * res.delete(0, res.length());
+ * int numPounds = (pct + 9) / 10;
+ * for (int i = 0 ; i != numPounds ; i++) {
+ * res.append('#');
+ * }
+ * while (res.length() != 10) {
+ * res.append(' ');
+ * }
+ * return res.toString();
+ * }
+ * 
+ * public void loadBar() throws InterruptedException{
+ * for (int i = 0 ; i <= 100 ; i+=Math.random() *8) {
+ * Thread.sleep(50);
+ * System.out.print(String.format("[%s]%d%%\r", progress(i), i));
+ * }
+ * }
+ * }
+ */
